ada40d52a60aca0187457072df0e9926d6769608
Manika Malhotra <manika@signoz.io>
2026-03-06 12:35:23 +0530

    chore: set prefetch false for links (#2837)
    * css: remove unused
    
    * remove more from tailwind.css
    
    * remove more unused classes from tailwind
    
    * remove unused classes from tailwind
    
    * fix: remove tailwind and global imports from route handlers, fix tailwind classname priorities
    
    * PageFeedback styling - remove ! override and use twMerge
    
    * convert images from png to webp
    
    * images: reduce webp quality
    
    * chore: use fill instead of explicit height and width for homepage hero
    
    * fix: homepage image - remove object-cover
    
    * fix: remove object-cover from video modal player
    
    * init: set prefetch false in links
    
    * fix: revert docs/introduction to docs
    
    * chore: add prefetch false in more places

--- components/GetStarted/index.tsx
+++ components/GetStarted/index.tsx
@@ -39,6 +39,7 @@ export const GetStarted = ({ page }) => {
                   clickName="Read Documentation Link"
                   clickText="Read Documentation"
                   clickLocation="Get Started Section"
+                  prefetch={false}
                 >
                   <Button
                     type={Button.TYPES.SECONDARY}
